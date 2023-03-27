5b95c6b76b520992bc418f7e0be36b7d694d8d92
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2023-03-28 00:26:47 +0700

    v2.1

--- service.sh
+++ service.sh
@@ -155,22 +155,57 @@ until [ "`getprop sys.boot_completed`" == "1" ]; do
   sleep 10
 done
 
+# function
+grant_permission() {
+pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
+pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
+if [ "$API" -ge 29 ]; then
+  pm grant $PKG android.permission.ACCESS_MEDIA_LOCATION 2>/dev/null
+  appops set $PKG ACCESS_MEDIA_LOCATION allow
+fi
+if [ "$API" -ge 33 ]; then
+  pm grant $PKG android.permission.READ_MEDIA_AUDIO
+  pm grant $PKG android.permission.READ_MEDIA_VIDEO
+  pm grant $PKG android.permission.READ_MEDIA_IMAGES
+  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
+fi
+appops set $PKG LEGACY_STORAGE allow
+appops set $PKG READ_EXTERNAL_STORAGE allow
+appops set $PKG WRITE_EXTERNAL_STORAGE allow
+appops set $PKG READ_MEDIA_AUDIO allow
+appops set $PKG READ_MEDIA_VIDEO allow
+appops set $PKG READ_MEDIA_IMAGES allow
+appops set $PKG WRITE_MEDIA_AUDIO allow
+appops set $PKG WRITE_MEDIA_VIDEO allow
+appops set $PKG WRITE_MEDIA_IMAGES allow
+if [ "$API" -ge 30 ]; then
+  appops set $PKG MANAGE_EXTERNAL_STORAGE allow
+  appops set $PKG NO_ISOLATED_STORAGE allow
+  appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+fi
+if [ "$API" -ge 31 ]; then
+  appops set $PKG MANAGE_MEDIA allow
+fi
+PKGOPS=`appops get $PKG`
+UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 userId= | sed 's/    userId=//'`
+if [ "$UID" -gt 9999 ]; then
+  appops set --uid "$UID" LEGACY_STORAGE allow
+  if [ "$API" -ge 29 ]; then
+    appops set --uid "$UID" ACCESS_MEDIA_LOCATION allow
+  fi
+  UIDOPS=`appops get --uid "$UID"`
+fi
+}
+
 # grant
 PKG=com.miui.misound
 pm grant $PKG android.permission.READ_PHONE_STATE
 pm grant $PKG android.permission.RECORD_AUDIO
 appops set $PKG SYSTEM_ALERT_WINDOW allow
-if [ "$API" -ge 30 ]; then
-  appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+if [ "$API" -ge 31 ]; then
+  pm grant $PKG android.permission.BLUETOOTH_CONNECT
 fi
-if [ "$API" -ge 33 ]; then
-  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
-fi
-PKGOPS=`appops get $PKG`
-UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 userId= | sed 's/    userId=//'`
-if [ "$UID" -gt 9999 ]; then
-  UIDOPS=`appops get --uid "$UID"`
-fi
+grant_permission
 
 # grant
 PKG=com.dolby.daxservice
